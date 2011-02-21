b7a037f760ac404643c23fef9b3ace7806afdd1d
matryer@mac.com <matryer@mac.com@5f548dbd-45b1-9705-3ecd-c0b5f49eef55>
2011-02-21 11:02:23 +0000

    Started adding request object (and supporting types)

---
+++ ios/sdk/stretchrsdk/stretchrsdktests/StretchrRequestTest.h
@@ -0,0 +1,18 @@
+//
+//  StretchrRequestTest.h
+//  stretchrsdk
+//
+//  Created by Mat Ryer on 21/Feb/2011.
+//  Copyright 2011 Borealis Web Ltd. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import <UIKit/UIKit.h>
+#import "Stretchr.h"
+
+@interface StretchrRequestTest : SenTestCase {
+    
+}
+
+
+@end
