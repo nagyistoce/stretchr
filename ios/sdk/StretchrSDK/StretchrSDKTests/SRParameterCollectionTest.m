c92f454eff940a8af2b911f8dd5fc0aafcbc24d6
matryer@mac.com <matryer@mac.com@5f548dbd-45b1-9705-3ecd-c0b5f49eef55>
2011-05-28 14:25:03 +0000

    tweaked copyright comment

--- ios/sdk/StretchrSDK/StretchrSDKTests/SRParameterCollectionTest.m
+++ ios/sdk/StretchrSDK/StretchrSDKTests/SRParameterCollectionTest.m
@@ -3,7 +3,7 @@
 //  XapiSdk
 //
 //  Created by Mat Ryer on 28/May/2011.
-//  Copyright 2011 Borealis Web Ltd. All rights reserved.
+//  Copyright 2011 Stretchr.com. All rights reserved.
 //
 
 #import "SRParameterCollectionTest.h"
