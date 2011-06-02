efe39168631339d3f1c736ca4db2cf33bb47e120
matryer@mac.com <matryer@mac.com@5f548dbd-45b1-9705-3ecd-c0b5f49eef55>
2011-06-02 14:29:56 +0000

    minor project updates

--- trunk/ios/sdk/StretchrSDK/StretchrSDKTests/TestHelper.h
+++ trunk/ios/sdk/StretchrSDK/StretchrSDKTests/TestHelper.h
@@ -3,7 +3,7 @@
 //  StretchrSDK
 //
 //  Created by Mat Ryer on 29/May/2011.
-//  Copyright 2011 Borealis Web Ltd. All rights reserved.
+//  Copyright 2011 Stretchr.com. All rights reserved.
 //
 
 #define STAssertEqualStrings(actual, expected, message) STAssertTrue([expected isEqualToString:actual], @"%@Strings do not match, expected: \"%@\", actual: \"%@\".", message ? @"" : [NSString stringWithFormat:@"%@ - ", message], expected, actual)
