391987f80b3c39478903df22d046d5d2afb3cf14
matryer@mac.com <matryer@mac.com@5f548dbd-45b1-9705-3ecd-c0b5f49eef55>
2011-06-02 14:17:20 +0000

    Added views for request and response

---
+++ trunk/ios/sdk/StretchrSDK/StretchrSDK/RequestViewController.h
@@ -0,0 +1,16 @@
+//
+//  RequestViewController.h
+//  StretchrSDK
+//
+//  Created by Mat Ryer on 2/Jun/2011.
+//  Copyright 2011 Borealis Web Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RequestViewController : UIViewController {
+    
+}
+
+@end
---
+++ trunk/ios/sdk/StretchrSDK/StretchrSDK/ResponseViewController.h
@@ -0,0 +1,16 @@
+//
+//  ResponseViewController.h
+//  StretchrSDK
+//
+//  Created by Mat Ryer on 2/Jun/2011.
+//  Copyright 2011 Borealis Web Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ResponseViewController : UIViewController {
+    
+}
+
+@end
